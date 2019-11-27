c88305601a84b69b6551e1b1aad2c9d1025ccbf7
Kyle <kyleyan@cs.uw.edu>
2019-11-26 19:22:29 -0800

    Initial importance code

--- code/importance.py
+++ code/importance.py
@@ -25,7 +25,7 @@ class PacSumExtractorWithImportance:
                 continue
 
             # edge_scores = self._calculate_similarity_matrix(article)
-            article_importance = self._calculate_article_importance(article)
+            article_importance = self._calculate_all_sentence_importance(article)
             ids: List[int] = self._select_tops(article_importance)
             summary = list(map(lambda x: article[x], ids))
 
@@ -34,7 +34,15 @@ class PacSumExtractorWithImportance:
 
         result = evaluate_rouge(summaries, references, remove_temp=True, rouge_args=[])
 
-    def _calculate_article_importance(self, article: List[str]) -> List[float]:
+    def _select_tops(self, article_importance: List[float]) -> List[int]:
+        id_importance_pairs: List[Tuple[int, float]] = []
+        for i in range(len(article_importance)):
+            id_importance_pairs.append((i, article_importance[i]))
+        id_importance_pairs.sort(key=lambda x: x[1], reverse=True)
+        extracted = [item[0] for item in id_importance_pairs[:self.extract_num]]
+        return extracted
+
+    def _calculate_all_sentence_importance(self, article: List[str]) -> List[float]:
          return [self._calculate_sentence_importance(idx, article)
                  for idx, sen in enumerate(article)]
 
@@ -47,17 +55,10 @@ class PacSumExtractorWithImportance:
                 # format sentences for masked LM
                 sentence_pairs, masked_lm_labels, loss_mask = self._generate_sentence_pairs(si, sj)
                 # TODO: What is P_BERT(s_j | s_i)
+                # TODO: Is the NLL loss just -sum(log P(w_l | s_i + s_j - w_l))?
                 loss = self._masked_lm(sentence_pairs, masked_lm_labels=masked_lm_labels)[0]
                 s_importance += loss
         return s_importance
-
-    def _select_tops(self, article_importance: List[float]) -> List[int]:
-        id_importance_pairs: List[Tuple[int, float]] = []
-        for i in range(len(article_importance)):
-            id_importance_pairs.append((i, article_importance[i]))
-        id_importance_pairs.sort(key=lambda x: x[1], reverse=True)
-        extracted = [item[0] for item in id_importance_pairs[:self.extract_num]]
-        return extracted
 
     def _generate_sentence_pairs(self, si: str, sj: str) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         # sj_masked_copies: [sj_len * sj_len]
@@ -78,8 +79,8 @@ class PacSumExtractorWithImportance:
         eos_copies = torch.zeros(sj_len, 1).fill_(self._tokenizer.eos_token_id).long()
 
         sentence_pairs = torch.cat((bos_copies, si_copies, eos_copies,
-                                    eos_copies, sj_masked_copies, eos_copies)
-                                   , 1).to(self.device)
+                                    eos_copies, sj_masked_copies, eos_copies),
+                                   1).to(self.device)
 
         # masked_lm_labels = torch.cat((torch.zeros(sj_len, si_len + 3).fill_(-1).long(),
         #                               sj_masked_labels,
@@ -88,8 +89,8 @@ class PacSumExtractorWithImportance:
 
         loss_mask = torch.cat((torch.zeros(sj_len, si_len + 3).bool(),
                                mask,
-                               torch.zeros(sj_len, 1).bool())
-                              , 1).to(self.device)
+                               torch.zeros(sj_len, 1).bool()),
+                              1).to(self.device)
 
         masked_lm_labels = torch.zeros_like(loss_mask).fill_(-1).long().masked_scatter(loss_mask, sj_copies)
 
